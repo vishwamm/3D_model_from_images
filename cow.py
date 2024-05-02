916cb9f598c11ff154d9b170599f182d474cc900
vishwamm <vishwa2455@gmail.com>
2024-05-02 21:48:26 +0530

    first commit

---
+++ cow.py
@@ -0,0 +1,4 @@
+import wget
+wget.download("https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow.obj")
+wget.download("https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow.mtl")
+wget.download("https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow_texture.png")
